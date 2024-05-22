9f364dcbb2451a4023bd6b76a901b005ae84ce51
Andrew5057 <84680088+Andrew5057@users.noreply.github.com>
2024-05-22 17:06:58 -0500

    Add files via upload

---
+++ dodgeball-playable/PlayDodgeball-win.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+TIMEOUT /T 1 >nul
+ECHO Welcome to Dodgeball!
+ECHO:
+TIMEOUT /T 2 >nul
+ECHO This is a lawless land, so yall are gonna have to make up the laws yourself.
+ECHO:
+TIMEOUT /T 3 >nul
+ECHO I accept no responsibility if this bricks your computer, sets your desk on fire, or causes any other property damage.
+ECHO:
+TIMEOUT /T 3 >nul
+java -cp . dodgeball.client.PlayDodgeball
