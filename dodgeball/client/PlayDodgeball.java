541e78b69298018b746facd37fddd59cfe5c1717
Andrew5057 <yimandrew27@gmail.com>
2024-05-24 01:17:40 -0500

    Game start messages

--- dodgeball/client/PlayDodgeball.java
+++ dodgeball/client/PlayDodgeball.java
@@ -8,7 +8,22 @@ import java.io.IOException;
  * Join a game of dodgeball.
  */
 public class PlayDodgeball {
+  /**
+   * Play a game of dodgeball.
+   *
+   * @param args Command line args
+   * @throws AWTException If a drawing fails
+   * @throws FileNotFoundException If modeling fails
+   * @throws IOException If server communications fail
+   */
   public static void main(String[] args) throws AWTException, FileNotFoundException, IOException {
+    System.out.println("Welcome to Dodgeball!\n");
+    System.out.println("There are no rules here, so yall are gonna have to make up the rules "
+        + "yourselves.\n");
+    System.out.println("I accept no responsibility if this bricks your computer, sets your desk "
+        + "on fire, or causes any other property damage.\n");
+    System.out.println("GLHF!\n");
+    
     Client client = new Client();
     new Thread(client).start();
   }
--- dodgeball/server/RunDodgeball.java
+++ dodgeball/server/RunDodgeball.java
@@ -4,7 +4,14 @@ package dodgeball.server;
  * Host a game of Dodgeball from this computer.
  */
 public class RunDodgeball {
+  /**
+   * Host a game of dodgeball.
+   *
+   * @param args Command line arguments
+   */
   public static void main(String[] args) {
+    System.out.println("Now hosting a game of dodgeball. Thank you for your sacrifice!\n");
+    
     GameManager manager = new GameManager();
     new Thread(manager).start();
   }
